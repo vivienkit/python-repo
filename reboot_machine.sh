8d7f7158374ac71d4f5078309bbcde42af959c19
vivienkit <kit038@gmail.com>
2018-03-07 11:54:42 +0100

    modifs pour logguer la console de debug de l'équipement lors d'un boot qui a merdé

--- reboot_machine.sh
+++ reboot_machine.sh
@@ -2,44 +2,68 @@
 
 ###### Initialisation des variables ######
 
-# fichier de log
-log="/home/pi/log_reboot.txt"
 # IP de la machine à tester
-IP='192.168.100.12'
+IP='192.168.100.88'
 # temps en s au bout duquel la machine testée doit avoir booté
-boot_time=10
+boot_time=80
 # temps de pause entre 2 reboots
-wait_time=5
+wait_time=10
 # variable nrs -> nbre de reboots souhaités
-nrs=20
-# date/heure formattée
-heure="date +'%D-%H.%M.%S'"
+nrs=2
+# date/heure formattée pour entrée log
+heure1="date +'%D-%H.%M.%S'"
+# date/heure formattée pour nom de fichier
+heure2="date +'%Hh%M'"
 # nbre de boots réussis
 OK=0
 # nbre de boots ratés
 NOK=0
 
+##########  fichiers de logs #########
+# répertoire des logs
+logDir="/home/pi/logs_reboots" 
+# fichier de log machine
+log_machine="$logDir/log_reboot.txt"
+# fichier temporaire de log console série
+log_temp="$logDir/tmp_log_serial.txt"
+# fichier daté de log console série
+log_serial="$logDir/fail_boot_`date +'%Hh%M'`.txt"
+
 # initialisation du GPIO 17 comme sortie
 gpio mode 0 output
+gpio write 0 0
 
-echo "`$heure` : Démarrage du script" >> $log
+# initialisation de l'adaptateur RS232/USB
+chmod +rw /dev/ttyUSB0
 
-for i in `seq 1 $nrs`; do
-  gpio write 0 1 # alim ON
-  echo "`$heure` : boot start" >> $log
-  sleep $boot_time
-  test_ping=$(ping -n -c 1 $IP -W 1 | grep "1 received") > /dev/null
-  if [[ $test_ping != "" ]]; then 
-	echo "`$heure` : OK La machine testée a correctement booté" >> $log
+# fonction pour logguer la sortie console de l'équipement en cas de non démarrage
+log_serial()
+{	ttylog -b 115200 -d /dev/ttyUSB0 > $log_temp &
+	sleep $1
+	pkill ttylog
+}
+
+echo "`$heure1` : Démarrage du script" >> $log_machine
+
+for i in `seq 1 $nrs`
+do
+  gpio write 0 1
+  echo "`$heure1` : boot start" >> $log_machine
+  log_serial $boot_time
+#  sleep $boot_time
+  test_ping=$(ping -n -c 1 $IP -W 1 | grep "0 received") > /dev/null
+  if [[ $test_ping = "" ]]
+    then 
+	echo "`$heure1` : OK La machine testée a correctement booté" >> $log_machine
 	((OK+=1))
 	else 
-	echo "`$heure` : NOK boot FAIL !" >> $log
+	echo "`$heure1` : NOK boot FAIL ! see $log_serial" >> $log_machine
+	cp $log_temp $logDir/fail_boot_`date +%Hh%M`.txt
 	((NOK+=1))
   fi
-  gpio write 0 0 # alim OFF
+  gpio write 0 0
   sleep $wait_time
 done
-
-echo "`$heure` : nbre de boots réussis : $OK, nbre de boots manqués : $NOK" >> $log
+echo "`$heure1` : nbre de boots réussis : $OK, nbre de boots manqués : $NOK" >> $log_machine
 
 exit 0
